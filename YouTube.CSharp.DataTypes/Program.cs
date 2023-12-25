1c7aef26a901efee1a11a413d618989b663b5677
GitUser <gituser@gmail.com>
2023-12-25 15:37:09 +0000

    Byte,Short,Int,Long Lessons

---
+++ YouTube.CSharp.DataTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿// Byte
+// Short
+// Int
+// Long
+// Enum
+
+// Byte => 8 bit (1 byte)
+byte numberOne = 5;
+byte maxByte = byte.MaxValue;
+byte minByte = byte.MinValue;
+
+//Console.WriteLine("Max Byte is {0}, Min Byte is {1}, Curren Byte Value is {2}",maxByte,minByte,numberOne);
+Console.WriteLine($"Max Byte is {maxByte}, Min Byte is {minByte}, Curren Byte Value is {numberOne}");
+
+
+// Short => 16 bit (2 byte)
+short numberTwo = 5;
+short maxShort = short.MaxValue;
+short minShort = short.MinValue;
+
+Console.WriteLine($"Max Short is {maxShort}, Min Short is {minShort}, Curren Short Value is {numberTwo}");
+
+// Int => 32 bit (4 byte)
+int numberThree = 25;
+int maxInt = int.MaxValue;
+int minInt = int.MinValue;
+
+Console.WriteLine($"Max Int is {maxInt}, Min Int is {minInt}, Curren Int Value is {numberThree}");
+
+// Long => 64 bit (8 byte)
+long numberFour = 25;
+long maxLong = long.MaxValue;
+long minLong = long.MinValue;
+
+Console.WriteLine($"Max Long is {maxLong}, Min Long is {minLong}, Curren Long Value is {numberFour}");
