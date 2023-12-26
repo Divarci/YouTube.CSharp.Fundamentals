1dcc37819128f17f39f20d84ea7d1a3fc644f490
GitUser <gituser@gmail.com>
2023-12-26 14:34:48 +0000

    Calculation Lessons

---
+++ YouTube.CSharp.CalculationsAndOperators/Program.cs
@@ -0,0 +1,83 @@
+﻿// CALCULATIONS AND OPERATORS
+
+//Calculations => + , - , / , * , %
+
+byte byteOne = 5;
+byte byteTwo = 10;
+
+byte resultByte =(byte)(byteOne * byteTwo);
+int newResultInt = byteOne * byteTwo;
+var commonResultForByte = byteOne * byteTwo;
+
+Console.WriteLine($"I am Byte {resultByte}, I am int {newResultInt}, i am here with var keyword {commonResultForByte}");
+//----
+
+short shortOne = 500;
+short shortTwo = 250;
+
+short resultShort = (short)(shortOne - shortTwo);
+int newResultIntTwo = shortOne - shortTwo;
+var commonResultForShort = shortOne - shortTwo;
+
+Console.WriteLine($"I am Short {resultShort}, I am int {newResultIntTwo}, i am here with var keyword {commonResultForShort}");
+//----
+
+int intOne = 60000;
+int intTwo = 45000;
+
+int resultInt = intOne + intTwo;
+long resultLongFromInt = intOne + intTwo;
+
+Console.WriteLine($"I am int {resultInt}, I am long {resultLongFromInt}");
+//----
+
+long longOne = 125000;
+long longTwo = 25000;
+
+long resultLong = longOne / longTwo;
+int resultInfFromLong = (int)(longOne / longTwo);
+int resultInfFromLongTwo = Convert.ToInt32(longOne / longTwo);
+
+Console.WriteLine($"I am long {resultLong}, I am int by casting {resultInfFromLong}, I am int by converting {resultInfFromLongTwo}");
+
+
+//--
+var newCalculationsWithMixedDataTypes = byteOne * shortTwo + intOne / longTwo - intTwo;
+                                        // 5    *   250    + 60000  / 25000   - 45000
+                                        //    1250         +   2              - 45000
+
+Console.WriteLine(newCalculationsWithMixedDataTypes);// -43748
+
+var newCalculationsWithMixedDataTypesTwo = byteOne * (shortTwo + intOne) / longTwo - intTwo;
+                                            // 5   *       60250         / 25000   - 45000
+                                            //                  12                 - 45000
+                                            
+Console.WriteLine(newCalculationsWithMixedDataTypesTwo);// -44988
+
+
+//--
+
+string newText = "25";
+
+var newResultWithString = newText + " " +shortOne;
+                       //  25     +   500
+Console.WriteLine(newResultWithString);//"25""500"
+
+var newResultWithStringTwo = short.Parse(newText) - shortTwo;
+
+Console.WriteLine(newResultWithStringTwo);//-225
+
+
+//--
+
+string newTextTwo = "Book";
+
+string sentence = "I bought " + newText+ " "+ newTextTwo+"s " + "last week";
+
+Console.WriteLine(sentence);
+
+
+
+
+
+
