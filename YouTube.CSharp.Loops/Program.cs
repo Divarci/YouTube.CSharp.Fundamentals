40704362f36c28f769579d0a4cdfd878145abb14
GitUser <gituser@gmail.com>
2023-12-27 12:16:35 +0000

    Loop Lessons

---
+++ YouTube.CSharp.Loops/Program.cs
@@ -0,0 +1,52 @@
+﻿//LOOPS
+
+//For
+//While
+//Do While
+//ForEach
+
+//For
+/*
+for (int i = 0; i < 15; i++)
+{
+    Console.WriteLine(i);
+}
+
+string[] students = { "Michael", "Bob", "Amanda" };
+
+for (int i = 0;i < students.Length; i++)
+{
+    Console.WriteLine(students[i]);
+}
+
+//While
+
+int number = 36;
+
+while (number<=100)
+{
+    Console.WriteLine(number);
+    number++;
+}
+
+
+//Do While
+
+do
+{
+    Console.WriteLine(number);
+    number++;
+
+} while (number <= 100);
+*/
+
+//ForEach
+
+string[] students = { "Michael", "Bob", "Amanda" };
+
+foreach (var item in students)
+{
+    Console.WriteLine(item);
+
+}
+
