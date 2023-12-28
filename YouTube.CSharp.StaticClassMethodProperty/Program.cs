3d413f0b475280117631b05faa8296652c4202b9
GitUser <gituser@gmail.com>
2023-12-28 23:55:40 +0000

    Static Class-Method-Property

---
+++ YouTube.CSharp.StaticClassMethodProperty/Program.cs
@@ -0,0 +1,20 @@
+﻿// STATIC CLASS-METHOD-PROPERTY
+
+
+//Example test = new Example();
+//test.Method();
+
+Example.Method();
+Example.Name = "Test";
+Console.WriteLine(Example.Name);
+
+
+static class Example
+{
+    public static string Name { get; set; }
+
+    public static void Method()
+    {
+        Console.WriteLine( " I am a method");
+    }
+}
