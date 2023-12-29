515400e97d3b2e18afd192fce93aa8f9403f9b36
GitUser <gituser@gmail.com>
2023-12-29 16:20:53 +0000

    Referance And Value Types

---
+++ Addition_ReferanceAndValueType_Lessons/Program.cs
@@ -0,0 +1,23 @@
+﻿// REFERANCE AND VALUE TYPES
+
+////value type
+//int number = 5;
+
+//int number2 =8;
+
+//number2 = number;
+
+//number = 7;
+
+//Console.WriteLine(number2);//5
+
+//referance type
+
+string[] test = new string[3] { "a", "b", "c" }; // m bc
+string[] test2 = new string[3] { "x", "y", "z" };
+
+test2 = test;// a b c
+
+test[0] = "m";
+
+Console.WriteLine(test2[0]);//m
