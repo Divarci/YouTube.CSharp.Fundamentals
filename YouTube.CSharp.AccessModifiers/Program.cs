68458a6078b9a67ab2ed60f7c699d912cdb08e34
GitUser <gituser@gmail.com>
2023-12-29 02:30:21 +0000

    Access Modifier Lessons

---
+++ YouTube.CSharp.AccessModifiers/Program.cs
@@ -0,0 +1,61 @@
+﻿//ACCESS MODIFIERS
+Service test = new Service();
+test.Add();
+//test.Update();
+//test.Remove();
+test.GetList();
+test.Select();
+
+
+
+public class Service
+{
+    public void Add() { }
+    private void Update() { }
+    protected void Remove() { }
+    protected internal void GetList() { }
+    internal void Select() { }
+
+
+
+    private void TestMethod()
+    {
+        Add();
+        Update();
+        Remove();
+        GetList();
+        Select();
+    }
+}
+
+
+
+class Example : Service
+{
+    public void MethodIntherit()
+    {
+        Add();
+      //Update();
+        Remove();
+        GetList();
+        Select();
+
+    }
+
+}
+
+
+class ExampleNormal
+{
+    Service test = new Service();
+
+    public void ExampleNorm()
+    {
+        test.Add();
+     // test.Update();
+     // test.Remove();
+        test.GetList();
+        test.Select();
+    }
+
+}
---
+++ YouTube.CSharp.ApplicationDemo/NewAssemblyClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTube.CSharp.ApplicationDemo
+{/*
+    public class NewAssemblyClass
+    {
+        Service test = new Service();
+
+        public void Method()
+        {
+            test.Add();
+            test.Update();
+            test.Remove();
+            test.GetList();
+            test.Select();
+        }
+
+
+    }
+
+
+    public class TestIntheritance : Service
+    {
+        public void MethodIntherit()
+        {
+            Add();
+            Update();
+            Remove();
+            GetList();
+            Select();
+
+        }
+
+    }*/
+}
