3f1f20ad5da905b4233ceb271365ba3c3fc10a1e
GitUser <gituser@gmail.com>
2024-01-01 14:26:56 +0000

    Manually Created Delegates

---
+++ YouTube.CSharp.Delegates/Program.cs
@@ -0,0 +1,85 @@
+﻿// DELEGATES
+
+//MANUALLY CREATED DELEGATES
+// Delegates appoint methods
+
+using Delegates;
+using YouTube.CSharp.Delegates;
+
+void Promotion(int reqExp, WorkersData data)
+{
+    foreach (var worker in data.workers)
+    {
+        if (worker.Experience > reqExp)
+        {
+            Console.WriteLine(worker.Name+": Promotion is Granted");
+        }
+        else
+        {
+            Console.WriteLine(worker.Name+": Promotion is Declined");
+        }
+    }
+
+    Console.WriteLine("---------------------");
+}
+
+void PromotionTemporary(int reqExp, WorkersData data)
+{
+    foreach (var worker in data.workers)
+    {
+        if (worker.Experience >= reqExp)
+        {
+            Console.WriteLine(worker.Name + ": Promotion is Granted");
+        }
+        else
+        {
+            Console.WriteLine(worker.Name + ": Promotion is Declined");
+        }
+    }
+    Console.WriteLine("---------------------");
+
+}
+
+WorkersData workersData = new WorkersData();
+
+//PromotionDelegate promotionDelegate = new PromotionDelegate(Promotion);
+//promotionDelegate(6, workersData);
+
+//PromotionDelegate promotionTemporaryDelegate = new PromotionDelegate(PromotionTemporary);
+//promotionTemporaryDelegate(2, workersData);
+
+//PromotionDelegate allPromotionDelegates = promotionDelegate + promotionTemporaryDelegate;
+//allPromotionDelegates(4,workersData);
+
+
+PromotionDelegate newPromotionDelegate = new PromotionDelegate((reqExp, data) =>
+{
+    foreach (var worker in data.workers)
+    {
+        if (worker.Experience > reqExp)
+        {
+            Console.WriteLine(worker.Name + ": Promotion is Granted");
+        }
+        else
+        {
+            Console.WriteLine(worker.Name + ": Promotion is Declined");
+        }
+    }
+
+    Console.WriteLine("---------------------");
+
+    Promotion(reqExp+1, workersData);
+});
+
+newPromotionDelegate(3, workersData);
+
+
+
+
+//BUILT IN DELEGATES
+
+
+namespace Delegates
+{
+    public delegate void PromotionDelegate(int reqExp, WorkersData data);
+}
---
+++ YouTube.CSharp.Delegates/Worker.cs
@@ -0,0 +1,24 @@
+﻿namespace YouTube.CSharp.Delegates
+{
+    public class Worker
+    {
+        public string Name { get; set; }
+        public int Experience { get; set; }
+        public int Salary { get; set; }
+    }
+
+    public class WorkersData
+    {
+        public List<Worker> workers = new()
+        {
+            new(){Name="Michael",Experience = 1,Salary = 1250},
+            new(){Name="Gordon",Experience = 2,Salary = 1450},
+            new(){Name="Amanda",Experience = 3,Salary = 1750},
+            new(){Name="Rachel",Experience = 4,Salary = 1950},
+            new(){Name="Jimmy",Experience = 5,Salary = 2250},
+            new(){Name="Bobby",Experience = 6,Salary = 2450},
+            new(){Name="James",Experience = 7,Salary = 2650},
+            new(){Name="Jack",Experience = 8,Salary = 2850}
+        };
+    }
+}
